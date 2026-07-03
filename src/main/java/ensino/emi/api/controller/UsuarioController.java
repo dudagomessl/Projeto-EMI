3191066256da87a6f75478f75781f4d9c9842acc
YuriPereiraFonseca <yuri.pereira.fonseca25@gmail.com>
2026-07-03 01:21:54 -0300

    Usando @PageableDefault para ter padrão + visualização do comando sql disparado pela JPA

--- src/main/java/ensino/emi/api/controller/UsuarioController.java
+++ src/main/java/ensino/emi/api/controller/UsuarioController.java
@@ -6,6 +6,9 @@ import ensino.emi.api.usuario.Usuario;
 import ensino.emi.api.usuario.UsuarioRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,8 +27,14 @@ public class UsuarioController {
         repository.save(new Usuario(dados));
     }
 
-    @GetMapping
-    public List<DadosListagemUsuario> listar() {
+    // Versão do get sem usar o page de organização
+    @GetMapping("/padrao")
+    public List<DadosListagemUsuario> listarPadrao() {
         return repository.findAll().stream().map(DadosListagemUsuario::new).toList();
+    }
+
+    @GetMapping
+    public Page<DadosListagemUsuario> listar(@PageableDefault(size = 5, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemUsuario::new);
     }
 }
